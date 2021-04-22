7448aed4e582c078bc0c7e8e8ec001f18f21f46d
NataliaQdla <kudla.natalia@gmail.com>
2021-04-22 13:41:20 +0200

    blog connected to contentful CMS

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -3,6 +3,7 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import blogStyles from "./blogTemplate.module.scss"
 import { renderRichText } from "gatsby-source-contentful/rich-text"
+import Head from "../components/head"
 
 export const query = graphql`
   query($slug: String!) {
@@ -11,19 +12,49 @@ export const query = graphql`
       publishedDate(formatString: "MMMM Do, YYYY")
       body {
         raw
+        references {
+          ... on ContentfulAsset {
+            contentful_id
+            __typename
+            fixed(width: 1600) {
+              width
+              height
+              src
+              srcSet
+            }
+          }
+        }
       }
     }
   }
 `
 
-const Blog = props => {
+const Blog = ({ data }) => {
+  const options = {
+    renderNode: {
+      "embedded-asset-block": node => {
+        return (
+          <>
+            <img
+              src={node.data.target.fixed.src}
+              alt={node.data.target.title}
+            />
+          </>
+        )
+      },
+    },
+  }
+
   return (
     <Layout>
-      <h1>{props.data.contentfulBlogPost.title}</h1>
+      <Head title={data.contentfulBlogPost.title} />
+      <h1>{data.contentfulBlogPost.title}</h1>
       <p className={blogStyles.blogDate}>
-        {props.data.contentfulBlogPost.publishedDate}
+        {data.contentfulBlogPost.publishedDate}
       </p>
-      {renderRichText(props.data.contentfulBlogPost.body)}
+      <div className={blogStyles.postContent}>
+        {renderRichText(data.contentfulBlogPost.body, options)}
+      </div>
     </Layout>
   )
 }
